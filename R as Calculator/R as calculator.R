72101231b395f7a99fdff06e03beb1346f994d55
ranzeet013 <128669620+ranzeet013@users.noreply.github.com>
2023-11-15 12:11:29 +0545

    calculator.R

---
+++ R as Calculator/R as calculator.R
@@ -0,0 +1,43 @@
+# Addition of numbers 
+
+12 + 20 
+
+# Subtract two number 
+
+10 -5 
+
+# Multiplication 
+
+10 * 5
+
+# Division 
+
+50 / 5
+
+# Parenthesis 
+
+100 / (50 / 10)
+
+# Square root 
+
+sqrt(10)
+
+# Exponential 
+
+exp(10)
+
+# Logarithm
+
+log(1)
+
+# logarithm of 10 t0 base 2
+
+log (10.2) 
+
+# Logarithm of 10 to base 1 
+
+log (10.1)
+
+(log(1)*10)
+
+10 / (sqrt(5)*2)
